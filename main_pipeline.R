e5930bd1a5529a5148e10abc4e21b1e2ba97b496
Antoine TOURRET <antoinetourretpro@gmail.com>
2025-04-27 17:19:56 +0200

    Commit 32
    main

--- main_pipeline.R
+++ main_pipeline.R
@@ -1,5 +1,4 @@
 # Load the pipeline script which, in turn, loads all other scripts including step 13.
 source("scripts/final_step_run_full_pipeline.R")
-
 # Now run the pipeline
 results_final <- run_full_pipeline()    
